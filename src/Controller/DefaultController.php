5eb28b3803680d61394480412bc58b0e1d88dc6b
Ferchichi Seifeddine <ferchichi.seifedine@gmail.com>
2019-05-14 16:03:14 +0200

    change some view in template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home_page_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('/index.html.twig', [
+        ]);
+    }
+}
+
+
+
+
+
